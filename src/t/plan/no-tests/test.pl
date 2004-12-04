994691f38924191f64608ed5b06d51fc093df7d5
nik <nik@2d5babbe-c3d5-0310-a406-eb661dfe2ac6>
2004-12-04 18:26:20 +0000

    Add a second test to test.t that ensures that the status code returned by the Perl and C test implementations are the same.
    Move t/plan_no_plan and t/plan_skip_all under t/plan.
    
    
    git-svn-id: svn://jc.ngo.org.uk/nik/libtap/trunk@207 2d5babbe-c3d5-0310-a406-eb661dfe2ac6

---
+++ src/t/plan/no-tests/test.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use Test::More;
+
+plan tests => 0;
+
+ok(1);
---
+++ src/t/plan/no_plan/test.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use Test::More qw(no_plan);
+
+ok(1);
