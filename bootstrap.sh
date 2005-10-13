8872ccb33b43675b985c825c8e204961fb7fbd24
nik <nik@2d5babbe-c3d5-0310-a406-eb661dfe2ac6>
2005-10-13 10:14:03 +0000

    Use generic versions of the autoconf toolchain if the version specific commands don't exist.
    Submitted by: Stig Brautaset <stig@brautaset.org>
    
    
    git-svn-id: svn://jc.ngo.org.uk/nik/libtap/trunk@352 2d5babbe-c3d5-0310-a406-eb661dfe2ac6

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 set -x
-aclocal19 -I /usr/local/share/aclocal || exit 1
-autoheader259 || exit 1
-libtoolize15 -c -f || exit 1
-automake19 -a -c || exit 1
-autoconf259 || exit 1
+aclocal19 -I /usr/local/share/aclocal || aclocal || exit 1
+autoheader259 || autoheader || exit 1
+libtoolize15 -c -f || libtoolize -c -f || glibtoolize -c -f || exit 1
+automake19 -a -c || automake -a -c || exit 1
+autoconf259 || autoconf || exit 1
