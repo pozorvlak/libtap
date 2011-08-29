76e18d5c64df74adeaa60e8bcf005562c8adc23b
Miles Gould <miles@assyrian.org.uk>
2011-08-29 12:21:53 +0100

    Fix bootstrap process so it builds; fix installation instructions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 set -x
 aclocal19 -I /usr/local/share/aclocal || aclocal || exit 1
-autoheader259 || autoheader || exit 1
+# autoheader259 || autoheader || exit 1
 libtoolize15 -c -f || libtoolize -c -f || glibtoolize -c -f || exit 1
 automake19 -a -c || automake -a -c || exit 1
 autoconf259 || autoconf || exit 1
